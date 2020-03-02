bf715dc2d21ebd2d155826ba24d4a058c1c07d88
Ivan <mivcamry@gmail.com>
2020-03-02 17:38:09 +0300

    Added ms sleeep function

--- src/bb_hal.c
+++ src/bb_hal.c
@@ -365,3 +365,24 @@ int delay_us(unsigned int time_us)
     }
 
 }
+
+int delay_ms(unsigned int msec)
+{
+    struct timespec ts;
+    int res;
+
+    if (msec < 0)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    ts.tv_sec = msec / 1000;
+    ts.tv_nsec = (msec % 1000) * 1000000;
+
+    do {
+        res = nanosleep(&ts, &ts);
+    } while (res && errno == EINTR);
+
+    return res;
+}
